1ddce158027bdabc08c818aae3fbab1d92e5d249
dsi <tanvermbstuit16@gmail.com>
2024-02-27 14:52:51 +0600

    users add

--- src/main/java/com/dsi/springbootbasic/controller/HomeController.java
+++ src/main/java/com/dsi/springbootbasic/controller/HomeController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HomeController {
 
@@ -11,6 +14,12 @@ public class HomeController {
     @ResponseBody
     public String home() {
         return "Welcome to Spring Boot Basic";
+    }
+
+    @GetMapping("/users")
+    @ResponseBody
+    public List<String> getAllUsers() {
+        return new ArrayList<>(List.of("Tanvert", "Ahammed", "Aminul", "Bari"));
     }
 
 }
